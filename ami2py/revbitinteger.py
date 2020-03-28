61daf1746de29307da78ffa7ab8a6f81e06b69c7
Dark Light <darkshadow200710@gmx.de>
2020-03-28 09:39:02 +0100

    deleted tox:travis from tox.ini

--- ami2py/revbitinteger.py
+++ ami2py/revbitinteger.py
@@ -11,16 +11,21 @@ from construct import (
     bytes2bits,
     int2byte,
     iterateints,
-    Construct,
     FormatField,
     FormatFieldError,
-    swapbitsinbytes
+    swapbitsinbytes,
 )
 
-def swapNibbles(x):
-    return ( (x & 0x0F)<<4 | (x & 0xF0)>>4 )
 
-SWAPBITSINBYTES_CACHE = {i:bits2bytes(bytes2bits(int2byte(swapNibbles(i)))) for i in range(256)}
+def swapNibbles(x):
+    return (x & 0x0F) << 4 | (x & 0xF0) >> 4
+
+
+SWAPBITSINBYTES_CACHE = {
+    i: bits2bytes(bytes2bits(int2byte(swapNibbles(i)))) for i in range(256)
+}
+
+
 def swapnibbleinbytes(data):
     r"""
     Performs a bit-reversal within a byte-string.
@@ -32,20 +37,22 @@ def swapnibbleinbytes(data):
     """
     return b"".join(SWAPBITSINBYTES_CACHE[b] for b in iterateints(data))
 
-class RevBitsInteger(BitsInteger):
 
+class RevBitsInteger(BitsInteger):
     def _parse(self, stream, context, path):
         length = self.length
         if callable(length):
             length = length(context)
         if length < 0:
             raise IntegerError("length must be non-negative")
-        data = stream_read(stream, length,"WhateverPath")
+        data = stream_read(stream, length, "WhateverPath")
         if self.swapped:
             if length & 7:
-                raise IntegerError("little-endianness is only defined for multiples of 8 bits")
+                raise IntegerError(
+                    "little-endianness is only defined for multiples of 8 bits"
+                )
             data = swapbytes(data)
-        data=data[::-1]
+        data = data[::-1]
         return bits2integer(data, self.signed)
 
     def _build(self, obj, stream, context, path):
@@ -61,7 +68,9 @@ class RevBitsInteger(BitsInteger):
         data = integer2bits(obj, length)
         if self.swapped:
             if length & 7:
-                raise IntegerError("little-endianness is only defined for multiples of 8 bits")
+                raise IntegerError(
+                    "little-endianness is only defined for multiples of 8 bits"
+                )
             data = swapbytes(data)
         data = data[::-1]
         stream_write(stream, data, length)
@@ -74,8 +83,8 @@ class RevFormatField(FormatField):
         super().__init__(endianity, format)
 
     def _parse(self, stream, context, path):
-        data = stream_read(stream, self.length,"WhateverPath")
-        data=swapbitsinbytes(data)
+        data = stream_read(stream, self.length, "WhateverPath")
+        data = swapbitsinbytes(data)
 
         try:
             return self.packer.unpack(data)[0]
@@ -86,8 +95,9 @@ class RevFormatField(FormatField):
         try:
             data = self.packer.pack(obj)
         except Exception:
-            raise FormatFieldError("struct %r error during building, given value %r" % (self.fmtstr, obj))
+            raise FormatFieldError(
+                "struct %r error during building, given value %r" % (self.fmtstr, obj)
+            )
         data = swapbitsinbytes(data)
         stream_write(stream, data, self.length)
         return obj
-
